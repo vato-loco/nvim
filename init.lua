8610291cef263b1dc5c9acf75de8d371804e1dd2
Gustaf Utas <gustaf.utas@polisen.se>
2024-11-29 12:25:47 +0100

    removed ensure installed tsserver

--- init.lua
+++ init.lua
@@ -24,9 +24,8 @@
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = 'f'
 vim.g.maplocalleader = 'f'
--- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
---
+
 -- [[ Setting options ]]
 require 'options'
 
@@ -36,8 +35,8 @@ require 'keymaps'
 -- [[ Install `lazy.nvim` plugin manager ]]
 require 'lazy-bootstrap'
 
--- [[ Configure and install plugins ]]
---  To update plugins you can run :Lazy update
+-- [[ Configure and install plugins ]] To update plugins you can run :Lazy update
 require 'lazy-plugins'
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -197,7 +197,6 @@ return {
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       'stylua', -- Used to format Lua code
-      'tsserver',
       'eslint',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
